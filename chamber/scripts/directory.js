07eac95220bdb4a5183c811db8e7de94c7862715
Adewemimo Emmanuel <eadewemimo@byupathway.edu>
2026-07-14 19:56:20 +0100

    save

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,4 @@
-const url = 'data/members.json';
+const url = 'scripts/members.json';
 const container = document.querySelector('#directory-container');
 const gridButton = document.querySelector('#gridBtn');
 const listButton = document.querySelector('#listBtn');
